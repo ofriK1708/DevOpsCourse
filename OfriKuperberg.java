9dff626d280e8b688dc75bedd5ae7c2ce14c6957
ofriK1708 <deatlyligthen@gmail.com>
2026-04-14 20:18:41 +0300

    ofri is very cool

--- OfriKuperberg.java
+++ OfriKuperberg.java
@@ -1,7 +1,7 @@
 public class OfriKuperberg{
 	public static void main(String[] args){
 		for(int i=0; i < 17; i++){
-			System.out.println("Ofri Kuperberg");
+			System.out.println("Ok is very cool");
 		}
 	}
 }	
